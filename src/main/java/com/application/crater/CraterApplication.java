e95fb7bbe98a71b7242ec6d006cd56a13542ad96
pedro.santos <pedroabs.dev42@gmail.com>
2021-01-28 20:42:08 -0300

    initial commit

---
+++ src/main/java/com/application/crater/CraterApplication.java
@@ -0,0 +1,13 @@
+package com.application.crater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/crater/CraterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.crater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
